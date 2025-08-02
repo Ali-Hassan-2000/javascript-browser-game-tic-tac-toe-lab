515c40e32e96f27aa0c106af3b352d738d07afb1
Dell <ali.hassan.salman.2000@gmail.com>
2025-08-02 21:14:45 +0300

    step 6

--- js/app.js
+++ js/app.js
@@ -1,20 +1,35 @@
 /*-------------------------------- Constants --------------------------------*/
-
-
+const squareEls = document.querySelectorAll('.sqr');
+const messageEl = document.querySelector('#message');
 
 /*---------------------------- Variables (state) ----------------------------*/
-
-
-
+const board = ['', '', '', '', '', '', '', '', ''];
+let turn = "X";
+let winner = false;
+let tie = false;
 /*------------------------ Cached Element References ------------------------*/
 
 
 
 /*-------------------------------- Functions --------------------------------*/
+const init = () => {
+    
+    
+    render();
+};
 
+const render = () => {
 
+};
+
+const updateBoard = () => {
+    board.forEach((cell, idx) => {
+        squareEls[idx].textContent = cell;
+    });
+};
 
 /*----------------------------- Event Listeners -----------------------------*/
 
 
 
+init();
