b4442eb52303a092001a8acd5a62399cb7a87e5b
Dell <ali.hassan.salman.2000@gmail.com>
2025-08-02 21:15:58 +0300

    step 6

--- js/app.js
+++ js/app.js
@@ -2,19 +2,33 @@
 const squareEls = document.querySelectorAll('.sqr');
 const messageEl = document.querySelector('#message');
 
+console.log(squareEls);
+console.log(messageEl);
 /*---------------------------- Variables (state) ----------------------------*/
 const board = ['', '', '', '', '', '', '', '', ''];
 let turn = "X";
 let winner = false;
 let tie = false;
+
+const winningCombos = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6]
+]
+
 /*------------------------ Cached Element References ------------------------*/
 
 
 
 /*-------------------------------- Functions --------------------------------*/
 const init = () => {
-    
-    
+
+
     render();
 };
 
@@ -25,9 +39,30 @@ const render = () => {
 const updateBoard = () => {
     board.forEach((cell, idx) => {
         squareEls[idx].textContent = cell;
+
+        if (cell === 'X') {
+            square.style.color = 'red';
+        } 
+        else if (cell === 'O') {
+            square.style.color = 'blue';
+        } 
+        else {
+            square.style.color = 'black';
+        }
     });
 };
 
+const updateMessage = () => {
+    if (!winner && !tie){
+        messageEl.textContent = `Player ${turn} turn`;
+    }
+    else if (!winner && tie){
+        messageEl.textContent = `Draw`;
+    }
+    else{
+        messageEl.textContent = `Cont. playing`;
+    }
+};
 /*----------------------------- Event Listeners -----------------------------*/
 
 
